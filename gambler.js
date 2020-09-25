19173578f362b056e8e96383f619c99c72b7bbe3
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-25 20:51:23 +0530

    Added: Ability to calculate everyday and total amount after 20 days

--- gambler.js
+++ gambler.js
@@ -2,11 +2,17 @@ const STAKE = 100;
 const BET =1;
 const UPPER_LIMIT = STAKE + (STAKE * 50 /100)
 const LOWER_LIMIT = STAKE - (STAKE * 50 /100)
+const TOTAL_DAYS = 20;
 
-cash = STAKE;
-var randomValue= Math.random()
-function gambleChecker()
+var cash=0
+var winningCash=0
+var totalAmount=0
+var dictionary={}
+
+function dailyGambling()
 {
+    winningCash=0
+    cash=STAKE
     while(cash != LOWER_LIMIT && cash != UPPER_LIMIT)
     {
         var randomValue= Math.random()   
@@ -19,5 +25,25 @@ function gambleChecker()
             cash = cash - BET
         }
     }
+    winningCash=cash-STAKE
+        return winningCash;
 }
-gambleChecker()
+function monthlyGambling()
+{
+    for(day = 1;day <= TOTAL_DAYS;day++)
+    {
+        var cash = dailyGambling()
+        dictionary[day] = cash
+        totalAmount = totalAmount + dictionary[day];
+        console.log("Amount of day:"+day +":"+dictionary[day])
+    }
+    if(totalAmount > 0)
+    {
+        console.log("Total  winning amount of 20 days:"+totalAmount)
+    }
+    else
+    {
+        console.log("Total  loosing amount of 20 days:"+totalAmount)
+    }
+}
+monthlyGambling()
