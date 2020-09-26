cc879328b88b98a0eb3a64eddfadf452378eee3c
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-26 10:33:06 +0530

    Refactored: Added proper documentatoin

--- gambler.js
+++ gambler.js
@@ -5,7 +5,7 @@ const STAKE = 100
 const BET =1
 const UPPER_LIMIT = STAKE + (STAKE * 50 /100)
 const LOWER_LIMIT = STAKE - (STAKE * 50 /100)
-const TOTAL_DAYS = 10
+const TOTAL_DAYS = 30
 
 /**
  * variables
@@ -17,6 +17,9 @@ var sumOfAmount={}
 
 
 class Gambler {
+/**
+ * Function to calculate daily gambling
+ */  
  dailyGambling()
 {
     winningCash=0
@@ -41,57 +44,62 @@ class Gambler {
  monthlyGambling()
 {
     do {
-     var array= new Array(5);
+     var array= new Array();
      var win = 0;
      var loose = 0;
      var winningData=0;
      var loosingData=0;
-    for(var day = 1;day <= TOTAL_DAYS;day++)
+    for(var day = 1;day<= TOTAL_DAYS;day++)
     {
         var cash = this.dailyGambling()
         sumOfAmount[day] = cash
         totalAmount = totalAmount + sumOfAmount[day];
-        console.log("Amount of day:"+day +":"+sumOfAmount[day])
+        console.log("Amount of day:"+day +"="+sumOfAmount[day])
     
-    if(cash==150)
-    {
-        console.log("Total  winning amount of 30 days: 50")
-          win=win+50;
-          winningData=winningData+50;
-         array[day-1]=win;
-         
+        if(cash==150)
+        {
+            win = win + 50;
+            winningData = winningData + 50;
+            array[day-1] = win;   
+        }
+        else
+        {
+            loose= loose-50;
+            loosingData=loosingData+50;
+            array[day-1]=loose; 
+        }
     }
-    else
-    {
-        console.log("Total  loosing amount of 30 days: 50")
-         loose= loose-50;
-         loosingData=loosingData+50;
-         array[day-1]=loose; 
-    }
-    }
-    if(winningData>loosingData){
-        console.log("Total Win is:"+(winningData-loosingData))
+    if(winningData > loosingData){
+        console.log("============================")
+        console.log("Total Win in a month:"+(winningData - loosingData))
+        console.log("============================")
     } else{
-        console.log("Total loss is:"+(loosingData-winningData))
+        console.log("============================")
+        console.log("Total loss in a month:"+(loosingData - winningData))
+        console.log("============================")
     }
+    
+    console.log("One month data")
+    console.log("=====================================================================================================================")
     console.log(array.toString())
-    var max=array[0];
-    var min=array[0];
-    var luckiestDay=0;
-    var unluckiestDay=0;
+    console.log("=====================================================================================================================")
+    var max = array[0];
+    var min = array[0];
+    var luckiestDay = 0;
+    var unluckiestDay = 0;
     for(var i=0; i<array.length;i++)
     {
-        if(max<array[i]){
-            max=array[i]
-            luckiestDay=i+1;
+        if(max < array[i]){
+            max = array[i]
+            luckiestDay = i+1;
         }
-        if(min>array[i]){
-            min=array[i]
-            unluckiestDay=i+1;
+        if(min > array[i]){
+            min = array[i]
+            unluckiestDay = i+1;
         }
     }
-    console.log("Luckiest Day:  "+luckiestDay+" He was in rupees "+max)
-    console.log("Unluckiest Day: "+unluckiestDay+" He was in rupees "+min)
+    console.log("Luckiest Day:  "+luckiestDay+" At Luckiest day he win "+max+" rupees")
+    console.log("Unluckiest Day: "+unluckiestDay+" At Unluckiest day he loose "+min+" rupees")
 } while (winningData>loosingData) 
 }
 }
