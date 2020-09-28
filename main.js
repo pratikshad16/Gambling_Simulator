9a2226825f7403702899655644459d0030661fcc
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-28 21:45:13 +0530

    Refactored: Code to take user input

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+const data = require('./Gambler')
+var readline = require('readline');
 class Main {
     main() {
         console.log("Welcome to Gambling Simulation Program");
-        const data = require('./Gambler')
-        data.luckyUnluckyDay(30);
+        var input = readline.createInterface(process.stdin, process.stdout);
+        input.question("Enter the number Of days :", (Days) => {
+            data.luckyUnluckyDay(Days);
+            process.exit();
+        })
     }
 }
 const obj = new Main();
